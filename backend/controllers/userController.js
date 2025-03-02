a96f09520ee8eb57b974b08893b0e2e02a3de9da
Xiechimon <xiechimon@qq.com>
2025-03-02 15:25:29 +0800

    Register new user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,49 @@
 // 处理请求的具体实现
-const registerUser = (req, res) => {
-  if (!req.body.email) {
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // 验证
+  if (!name || !email || !password) {
     res.status(400);
-    throw new Error("E");
+    throw new Error("请添加所有必填字段");
   }
-  res.send("用户注册");
-};
+  if (password.length < 6) {
+    res.status(400);
+    throw new Error("密码必须大于6位");
+  }
+
+  // 检查邮箱是否存在
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("该邮箱已被注册");
+  }
+
+  // 创建新用户
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    const { _id, _name, email, photo, phone, bio } = user;
+    res.status(201).json({
+      _id,
+      _name,
+      email,
+      photo,
+      phone,
+      bio,
+    });
+  } else {
+    res.status(400);
+    throw new Error("无效的用户数据");
+  }
+});
 
 module.exports = {
   registerUser,
