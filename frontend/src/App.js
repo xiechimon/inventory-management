dd55a7d7c1b179fdcd74bc585a76845274b76e15
Xiechimon <xiechimon@qq.com>
2025-03-07 23:37:05 +0800

    set up routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+
 function App() {
-  return (
-    <div>
-      <h1>HelloWorld</h1>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home/>} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
