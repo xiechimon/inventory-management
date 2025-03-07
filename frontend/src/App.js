b6fff7db9a6df947dd429e5615ae9e947fec8931
Xiechimon <xiechimon@qq.com>
2025-03-08 02:28:42 +0800

    del register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Auth from "./pages/auth/Auth";
-import Register from "./pages/auth/Register";
 
 function App() {
     return (
@@ -8,7 +7,6 @@ function App() {
             <Routes>
               <Route path="/" element={<Navigate to="/auth" replace />} />
               <Route path="/auth" element={<Auth/>} />
-              <Route path="/register" element={<Register/>} />
             </Routes>
         </BrowserRouter>
     );
