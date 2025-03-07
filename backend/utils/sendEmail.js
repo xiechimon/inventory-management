bcaf2f4757ce40b727fea3ea76b080a96d0f9b57
Xiechimon <xiechimon@qq.com>
2025-03-07 12:00:35 +0800

    create send email function

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,37 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
+    // 邮件发送
+    const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: 587,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
+        tls: {
+            rejectUnauthorized: false,
+        },
+    });
+
+    // 发送邮件的选项
+    const options = {
+        form: sent_from,
+        to: sent_to,
+        replyTo: reply_to,
+        subject: subject,
+        html: message,
+    };
+
+    // 发送
+    transporter.sendMail(options, function (err, info) {
+        if (err) {
+            console.log(err);
+        }
+        console.log(info);
+    });
+};
+
+module.exports = {
+    sendEmail,
+};
