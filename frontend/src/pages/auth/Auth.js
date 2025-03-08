cdbb21f9bb583594b8c8d243b2e3aa8940c8a7d7
Xiechimon <xiechimon@qq.com>
2025-03-08 21:20:10 +0800

    complete auth page

--- frontend/src/pages/auth/Auth.js
+++ frontend/src/pages/auth/Auth.js
@@ -27,6 +27,7 @@ const Auth = () => {
                         type="text"
                         className="input-field"
                         id="log-email"
+                        name="email"
                         required
                     />
                     <label htmlFor="log-email" className="label">
@@ -39,6 +40,7 @@ const Auth = () => {
                         type="password"
                         className="input-field"
                         id="log-pass"
+                        name="password"
                         required
                     />
                     <label htmlFor="log-pass" className="label">
@@ -53,15 +55,17 @@ const Auth = () => {
                     </div>
                 </div>
                 <div className="input-box">
-                    <button className="btn-submit" id="SignInBtn">
+                    <button className="btn-submit" id="SignInBtn" type="submit">
                         登录 <i className="bx bx-log-in"></i>
                     </button>
                 </div>
                 <div className="switch-form">
                     <span>
-                        没有账户？ 
+                        没有账户？
                         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                        <a href="#" onClick={registerLink}>注册</a>
+                        <a href="#" onClick={registerLink}>
+                            注册
+                        </a>
                     </span>
                 </div>
             </form>
@@ -73,6 +77,7 @@ const Auth = () => {
                         type="text"
                         className="input-field"
                         id="reg-name"
+                        name="name"
                         required
                     />
                     <label htmlFor="reg-name" className="label">
@@ -82,9 +87,10 @@ const Auth = () => {
                 </div>
                 <div className="input-box">
                     <input
-                        type="text"
+                        type="type"
                         className="input-field"
                         id="reg-email"
+                        name="email"
                         required
                     />
                     <label htmlFor="reg-email" className="label">
@@ -97,6 +103,7 @@ const Auth = () => {
                         type="password"
                         className="input-field"
                         id="reg-pass"
+                        name="password"
                         required
                     />
                     <label htmlFor="reg-pass" className="label">
@@ -104,11 +111,24 @@ const Auth = () => {
                     </label>
                     <i className="bx bx-lock-alt icon"></i>
                 </div>
+                <div className="input-box">
+                    <input
+                        type="password"
+                        className="input-field"
+                        id="reg-pass"
+                        name="password"
+                        required
+                    />
+                    <label htmlFor="reg-pass" className="label">
+                        确认密码
+                    </label>
+                    <i className="bx bxs-lock-alt icon"></i>
+                </div>
                 <div className="form-cols">
                     <div className="col-2"></div>
                 </div>
                 <div className="input-box">
-                    <button className="btn-submit" id="SignUpBtn">
+                    <button className="btn-submit" id="SignUpBtn" type="submit">
                         注册 <i className="bx bx-user-plus"></i>
                     </button>
                 </div>
@@ -116,7 +136,9 @@ const Auth = () => {
                     <span>
                         已拥有账户？
                         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                        <a href="#" onClick={loginLink}>登录</a>
+                        <a href="#" onClick={loginLink}>
+                            登录
+                        </a>
                     </span>
                 </div>
             </form>
