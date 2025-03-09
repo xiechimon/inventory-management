94636669f9e639a1130794bba20a4a5d9371f482
Xiechimon <xiechimon@qq.com>
2025-03-09 20:26:19 +0800

    fix: siderbar auto unfold when switch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,9 +8,7 @@ import SalesPage from "./pages/SalesPage";
 import OrdersPage from "./pages/OrdersPage";
 import AnalyticsPage from "./pages/AnalyticsPage";
 import SettingsPage from "./pages/SettingsPage";
-// 组件
-
-
+import Layout from "./components/common/Layout";
 
 function App() {
     return (
@@ -21,13 +19,15 @@ function App() {
                 <Route path="/auth" element={<Auth />} />
 
                 {/* 仪表盘 */}
-                <Route path="/overview" element={<OverviewPage />} />
-                <Route path="/products" element={<ProductsPage />} />
-                <Route path="/users" element={<UsersPage />} />
-                <Route path="/sales" element={<SalesPage />} />
-                <Route path="/orders" element={<OrdersPage />} />
-                <Route path="/analytics" element={<AnalyticsPage />} />
-                <Route path="/settings" element={<SettingsPage />} />
+                <Route element={<Layout />}>
+                    <Route path="/overview" element={<OverviewPage />} />
+                    <Route path="/products" element={<ProductsPage />} />
+                    <Route path="/users" element={<UsersPage />} />
+                    <Route path="/sales" element={<SalesPage />} />
+                    <Route path="/orders" element={<OrdersPage />} />
+                    <Route path="/analytics" element={<AnalyticsPage />} />
+                    <Route path="/settings" element={<SettingsPage />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
