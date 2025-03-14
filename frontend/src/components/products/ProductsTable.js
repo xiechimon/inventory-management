d4ec68f6d09c3425f40ebcad2e9adc7d3e1e2619
Xiechimon <xiechimon@qq.com>
2025-03-15 01:04:17 +0800

    add a pagination for product table

--- frontend/src/components/products/ProductsTable.js
+++ frontend/src/components/products/ProductsTable.js
@@ -1,8 +1,18 @@
 import { motion } from "framer-motion";
-import { Edit, Eye, Search, Trash2 } from "lucide-react";
+import {
+    AlignRight,
+    ArrowLeft,
+    ArrowRight,
+    ArrowRightCircle,
+    Edit,
+    Eye,
+    MoveRight,
+    Trash2,
+} from "lucide-react";
 import { useEffect, useState } from "react";
 import SearchBox from "../common/SearchBox";
 import { useDispatch, useSelector } from "react-redux";
+import ReactPaginate from "react-paginate";
 import {
     FILTER_PRODUCTS,
     selectFilterProducts,
@@ -21,6 +31,27 @@ const ProductsTable = ({ products }) => {
         }
         return text;
     };
+
+    // BEGIN Paginate
+    const [currentItems, setCurrentItems] = useState([]);
+    const [pageCount, setPageCount] = useState(0);
+    const [itemOffset, setItemOffset] = useState(0);
+    const itemsPerPage = 5; // 每页显示的数量
+
+    useEffect(() => {
+        const endOffset = itemOffset + itemsPerPage;
+
+        setCurrentItems(filteredProducts.slice(itemOffset, endOffset));
+        setPageCount(Math.ceil(filteredProducts.length / itemsPerPage));
+    }, [itemOffset, itemsPerPage, filteredProducts]);
+
+    const handlePageClick = (event) => {
+        const newOffset =
+            (event.selected * itemsPerPage) % filteredProducts.length;
+
+        setItemOffset(newOffset);
+    };
+    // END Paginate
 
     useEffect(() => {
         dispatch(FILTER_PRODUCTS({ products, search }));
@@ -76,13 +107,7 @@ const ProductsTable = ({ products }) => {
                         </thead>
 
                         <tbody className="divide-y divide-gray-300">
-                            {/* {filteredProducts.map((product) => (
-                                <motion.tr
-                                    key={product.id}
-                                    initial={{ opacity: 0 }}
-                                    animate={{ opacity: 1 }}
-                                    transition={{ duration: 0.3 }}
-                                >
+                            {/* 
                                     <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 flex gap-2 items-center">
                                         <img
                                             src="https://images.unsplash.com/photo-1627989580309-bfaf3e58af6f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8d2lyZWxlc3MlMjBlYXJidWRzfGVufDB8fDB8fHww"
@@ -91,31 +116,8 @@ const ProductsTable = ({ products }) => {
                                         />
                                         {product.name}
                                     </td>
-
-                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
-                                        {product.category}
-                                    </td>
-
-                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
-                                        ${product.price.toFixed(2)}
-                                    </td>
-                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
-                                        {product.stock}
-                                    </td>
-                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
-                                        {product.sales}
-                                    </td>
-                                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-700">
-                                        <button className="text-indigo-600 hover:text-indigo-700 mr-2">
-                                            <Edit size={18} />
-                                        </button>
-                                        <button className="text-red-600 hover:text-red-700">
-                                            <Trash2 size={18} />
-                                        </button>
-                                    </td>
-                                </motion.tr>
-                            ))} */}
-                            {filteredProducts.map((product, index) => {
+                           */}
+                            {currentItems.map((product, index) => {
                                 const { _id, name, category, price, quantity } =
                                     product;
                                 return (
@@ -165,6 +167,52 @@ const ProductsTable = ({ products }) => {
                         </tbody>
                     </table>
                 )}
+            </div>
+            <div className="mt-6">
+                {/* 
+                    containerClassName="flex items-center justify-center mt-8 space-x-2"
+                    pageClassName="w-10 h-10 flex items-center justify-center rounded border border-gray-200"
+                    pageLinkClassName="text-gray-700 hover:bg-gray-100"
+                    previousClassName="w-10 h-10 flex items-center justify-center rounded border border-gray-200"
+                    previousLinkClassName="text-gray-700 hover:bg-gray-100"
+                    nextClassName="w-10 h-10 flex items-center justify-center rounded border border-gray-200"
+                    nextLinkClassName="text-gray-700 hover:bg-gray-100"
+                    breakClassName="w-10 h-10 flex items-center justify-center"
+                    breakLinkClassName="text-gray-700"
+                    activeClassName="bg-blue-600 text-white"
+                /> */}
+                <ReactPaginate
+                    breakLabel={<span className="text-gray-300">...</span>}
+                    nextLabel={
+                        <div className="flex items-center group">
+                            <span className="text-gray-500 group-hover:text-gray-700">
+                                next
+                            </span>
+                            <ArrowRight className="ml-1 h-4 w-4 stroke-2 text-gray-400 group-hover:text-gray-600 transition-colors" />
+                        </div>
+                    }
+                    previousLabel={
+                        <div className="flex items-center group">
+                            <ArrowLeft className="mr-1 h-4 w-4 stroke-2 text-gray-400 group-hover:text-gray-600 transition-colors" />
+                            <span className="text-gray-500 group-hover:text-gray-700">
+                                prev
+                            </span>
+                        </div>
+                    }
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={2}
+                    pageCount={pageCount}
+                    renderOnZeroPageCount={null}
+                    containerClassName="flex justify-center items-center gap-3 font-medium"
+                    pageLinkClassName="px-2 py-1 text-gray-500 hover:text-gray-800 rounded-md transition-all duration-300 hover:bg-gray-100/30"
+                    previousLinkClassName="hover:bg-transparent" // 移除父级容器的背景
+                    nextLinkClassName="hover:bg-transparent"
+                    breakLinkClassName="text-gray-400 cursor-default"
+                    activeClassName="relative text-gray-900 before:absolute before:-bottom-1 before:left-1/2 before:w-5 before:h-0.5 before:bg-gray-300 before:-translate-x-1/2"
+                    disabledClassName="opacity-40 cursor-not-allowed"
+                    disabledLinkClassName="hover:bg-transparent"
+                    pageClassName="hover:-translate-y-0.5 transition-transform" // 悬停微浮动
+                />
             </div>
         </motion.div>
     );
